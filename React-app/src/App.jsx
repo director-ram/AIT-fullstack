eb3c79f79a8b852121158f08d9dc885f064e96dc
hemasai <charankram4@gmail.com>
2026-09-02 20:24:20 +0530

    feat: initialize React app with Vite, Tailwind CSS, and ESLint configuration, including basic components and styling

---
+++ React-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+    <h1 className='text-3xl font-bold underline text-red-500'>Hello World</h1>
+    <p className='text-lg text-blue-500'>This is a paragraph</p>
+    <button className='bg-blue-500 text-white p-2 rounded-md'>Click me</button>
+    </>
+  )
+}
+
+export default App
